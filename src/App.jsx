78e1b14bf0eabcbd70e7b94e63cd4dfb37aa1483
NTTai - PC09243 <nguyentantaivithanh@gmail.com>
2025-01-11 15:54:33 +0700

    Test - API

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
-import React, { useEffect, useState } from 'react';
-import { fetchHello } from './api';
+import React, { useEffect, useState } from "react";
+import { fetchUsers } from "./api";
+import userApi from "./api/userApi";
 
 function App() {
-  const [message, setMessage] = useState('');
+  const [user, setUser] = useState({});
 
   useEffect(() => {
-    fetchHello()
-      .then((response) => setMessage(response.data))
+    userApi
+      .getById(1)
+      .then((response) => setUser(response.data))
       .catch((error) => console.error(error));
   }, []);
 
   return (
     <div>
-      <h1>{message}</h1>
+      <h1>1</h1>
+      <p>{user.id} - {user.name} - {user.email}</p>
     </div>
   );
 }
