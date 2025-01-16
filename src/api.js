43cb2f0eee7eb60c2bc0e7df65f24a3d1adc0681
NTTai - PC09243 <nguyentantaivithanh@gmail.com>
2025-01-16 14:32:39 +0700

    Authentication

--- src/api.js
+++
@@ -1,5 +0,0 @@
-import axios from 'axios';
-
-const API = axios.create({ baseURL: 'https://e0c21987-e955-4814-88fa-ee71c91177d0.mock.pstmn.io' });
-
-export const fetchUsers = () => API.get("/users");
