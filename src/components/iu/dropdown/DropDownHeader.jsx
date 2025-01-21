98b9737dda50f7502433bc735cef83ee75479cab
NTTai - PC09243 <nguyentantaivithanh@gmail.com>
2025-01-21 14:33:02 +0700

    Update Authentication

---
+++ src/components/iu/dropdown/DropDownHeader.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Menu, Dropdown } from "antd";
+import { CaretDownOutlined } from "@ant-design/icons";
+import { useNavigate } from "react-router-dom";
+import scss from "./DropDownHeader.module.scss";
+
+const CustomDropdown = ({ menuItems, label }) => {
+  const navigate = useNavigate();
+
+  const menu = (
+    <Menu>
+      {menuItems.map((item, index) => (
+        <Menu.Item key={index} onClick={() => navigate(item.link)}>
+          {item.name}
+        </Menu.Item>
+      ))}
+    </Menu>
+  );
+
+  return (
+    <Dropdown overlay={menu} trigger={["click"]}>
+      <span className={scss.title}>
+        {label} <CaretDownOutlined />
+      </span>
+    </Dropdown>
+  );
+};
+
+export default CustomDropdown;
--- src/layouts/mainLayout/MainLayout.jsx
+++ src/layouts/mainLayout/MainLayout.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Header from "@components/header/Header";
 import Footer from "@components/footer/Footer";
+import scss from "./mainLayout.module.scss";
 
 const MainLayout = ({ children }) => {
   return (
     <>
       <Header />
-      <main>{children}</main>
+      <main className={scss["container-main"]}>{children}</main>
       <Footer />
     </>
   );
