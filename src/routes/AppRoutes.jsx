46f6a3fec24b6d2d7f1704a71fc7c40b58dc3b8a
NTTai - PC09243 <nguyentantaivithanh@gmail.com>
2025-02-13 15:59:37 +0700

    404, API, Decentralization

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -19,7 +19,10 @@ import SkillForm from "@pages/form/SkillForm";
 import StaffForm from "@pages/form/StaffForm";
 import ProfileFreelancers from "@pages/profileFreelancers/ProfileFreelancers";
 import Profile from "@pages/profile/Profile";
+<<<<<<< HEAD
 import ProfileRecruiters from "../pages/ProfileRecruiters/ProfileRecruiters";
+=======
+>>>>>>> d0e80b1 (404, API, Decentralization)
 import ProfileLayout from "@layouts/ProfileLayout/ProfileLayout";
 import LanguageTable from "@pages/language/LanguageTable";
 import LanguageForm from "@pages/language/LanguageForm";
@@ -178,6 +181,7 @@ const AppRoutes = () => {
               </MainLayout>
             }
           />
+<<<<<<< HEAD
         }
       />
       <Route
@@ -188,6 +192,8 @@ const AppRoutes = () => {
               <ProfileRecruiters />
             </ProfileLayout>
           </MainLayout>
+=======
+>>>>>>> d0e80b1 (404, API, Decentralization)
         }
       />
       <Route
