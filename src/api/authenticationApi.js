048f2349b952bb188dca368df6eebcc5034b0d87
xifff <minhtient87@gmail.com>
2025-02-19 20:47:31 +0700

    minhtienpro

--- src/api/authenticationApi.js
+++ src/api/authenticationApi.js
@@ -3,7 +3,7 @@ import apiClient from './index';
 const loginApi = {
     login: (email, password) =>
         apiClient.post('/login', { email, password }),
-    register: (email, password) => apiClient.post('/register', { email, password }),
+    register: (values) => apiClient.post('/register', { values }),
 };
 
 
