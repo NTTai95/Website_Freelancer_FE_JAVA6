c728ae3da547d12c93b8bd622cac9339389b2221
NguyenTNgocNghi <nnghi7867@gmail.com>
2025-02-25 15:35:44 +0700

    AboutAbout

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -12,27 +12,27 @@ import {
 const arrays = [
   {
     fullName: "Nguyễn Tấn Tài",
-    description: "Người lap trinh",
+    description: "Là một lập trình viên backend giàu kinh nghiệm, tôi chuyên thiết kế, phát triển và tối ưu hóa hệ thống backend, API, đảm bảo hiệu suất và bảo mật cao. Với kỹ năng quản lý nhóm và tư duy chiến lược, tôi thường xuyên đảm nhận vai trò leader, dẫn dắt đội ngũ hoàn thành dự án đúng tiến độ, tối ưu workflow và đảm bảo chất lượng mã nguồn. Tôi luôn tìm kiếm giải pháp tối ưu để cải thiện hệ thống và nâng cao trải nghiệm người dùng.",
   },
   {
     fullName: "Nguyễn Thị Ngọc Nghi",
-    description: "Người lap trinh",
+    description: "Là một lập trình viên frontend đam mê sáng tạo, tôi chuyên xây dựng giao diện trực quan, tối ưu trải nghiệm người dùng và phát triển các hiệu ứng tương tác mượt mà. Với nền tảng vững chắc về HTML, CSS, JavaScript và các framework hiện đại như Vue.js, React.js, tôi không chỉ tập trung vào giao diện mà còn đảm bảo logic frontend hoạt động hiệu quả, kết nối mượt mà với backend. ",
   },
   {
     fullName: "Nguyễn Long Nhi",
-    description: "Người lap trinh",
+    description: "Tôi chuyên về cơ sở dữ liệu và kết nối API, đảm bảo hệ thống lưu trữ dữ liệu an toàn, tối ưu và hoạt động hiệu quả. Với kinh nghiệm làm việc trên SQL, NoSQL và tối ưu truy vấn, tôi giúp duy trì luồng dữ liệu ổn định giữa backend và frontend, hỗ trợ hệ thống mở rộng linh hoạt",
   },
   {
     fullName: "Trần Minh Tiến",
-    description: "Người lap trinh",
+    description: "Tôi chuyên về lập trình, cơ sở dữ liệu, đảm bảo hệ thống lưu trữ và truy xuất dữ liệu nhanh, bảo mật. Thành thạo SQL, NoSQL và tối ưu truy vấn, tôi cũng chịu trách nhiệm kết nối API, giúp luồng dữ liệu hoạt động trơn tru giữa các hệ thống.",
   },
   {
     fullName: "Ngô Gia Huy",
-    description: "Người lap trinh",
+    description: "Là một chuyên gia về cơ sở dữ liệu, tôi chịu trách nhiệm thiết kế, tối ưu và quản lý hệ thống dữ liệu, đảm bảo hiệu suất và bảo mật cao. Tôi có kinh nghiệm làm việc với các hệ quản trị cơ sở dữ liệu như MySQL, PostgreSQL, SQL Server và NoSQL. Bên cạnh đó, tôi cũng thành thạo trong việc kết nối và tương tác với API, đảm bảo luồng dữ liệu giữa backend và frontend diễn ra mượt mà, ổn định. Tôi luôn tìm cách tối ưu truy vấn, cải thiện tốc độ xử lý dữ liệu và xây dựng hệ thống lưu trữ linh hoạt, mở rộng dễ dàng.",
   },
   {
-    fullName: "Nguyễn Khách Duy",
-    description: "Người lap trinh",
+    fullName: "Nguyễn Khánh Duy",
+    description: "Là một lập trình viên chuyên về cơ sở dữ liệu và kết nối hệ thống, tôi đảm nhận việc thiết kế, tối ưu hóa và bảo trì các hệ thống lưu trữ dữ liệu, đảm bảo hiệu suất cao và bảo mật chặt chẽ. Tôi có kinh nghiệm làm việc với các hệ quản trị như MySQL, PostgreSQL, SQL Server và NoSQL, giúp xây dựng kiến trúc dữ liệu linh hoạt, dễ mở rộng. Ngoài ra, tôi cũng phát triển và tối ưu API, đảm bảo luồng dữ liệu giữa các hệ thống backend và frontend diễn ra nhanh chóng, ổn định.",
   },
 ];
 function About() {
@@ -40,13 +40,6 @@ function About() {
     <div>
       {" "}
       <div className={"container"}>
-        <p
-          className={
-            "elegant-text text-center text-danger fs-5 mt-4 " + scss.title
-          }
-        >
-          Thành viên sáng lập công ty
-        </p>
         <Row gutter={16}>
           {arrays.map((item, index) => (
             <Col key={index} className={scss.div1} span={8}>
