a9a68a28a5d011cd72f5c00f4fddd794d1b67b87
NTTai - PC09243 <nguyentantaivithanh@gmail.com>
2025-04-11 21:35:21 +0700

    Fix - v5

--- src/pages/basic/listFreelancer/ListFreelancer.jsx
+++ src/pages/basic/listFreelancer/ListFreelancer.jsx
@@ -5,95 +5,97 @@ import { Skeleton, Row, Col, Pagination, Divider, Typography } from "antd";
 import profileApi from "@api/profileApi";
 
 const ListFreelancer = () => {
-  const [profile, setProfile] = useState(null);
-  const [search, setSearch] = useState("");
+    const [profile, setProfile] = useState(null);
+    const [search, setSearch] = useState("");
 
-  const [pagination, setPagination] = useState({
-    current: 1,
-    pageSize: 10,
-    total: 0,
-  });
-
-  const fetchData = async () => {
-    const res = await profileApi.getPageFreelancerNotNull({
-      page: pagination.current,
-      size: pagination.pageSize,
-      search: `fullName,${search}`,
+    const [pagination, setPagination] = useState({
+        current: 1,
+        pageSize: 10,
+        total: 0
     });
 
-    const { content, totalElements } = res.data;
+    const fetchData = async () => {
+        const res = await profileApi.getPageFreelancerNotNull({
+            page: pagination.current,
+            size: pagination.pageSize,
+            search: `fullName,${search}`
+        });
 
-    setProfile(content);
-    setPagination((prev) => ({
-      ...prev,
-      total: totalElements,
-    }));
-  };
+        const { content, totalElements } = res.data;
 
-  useEffect(() => {
-    fetchData();
-  }, [pagination.current, pagination.pageSize, search]);
+        setProfile(content);
+        setPagination(prev => ({
+            ...prev,
+            total: totalElements
+        }));
+    };
 
-  const handlePageChange = (page, pageSize) => {
-    setPagination((prev) => ({
-      ...prev,
-      current: page,
-      pageSize: pageSize,
-    }));
-    window.scrollTo(0, 0);
-  };
+    useEffect(() => {
+        fetchData();
+    }, [pagination.current, pagination.pageSize, search]);
 
-  const onSearch = (value) => {
-    setSearch(value);
-  };
+    const handlePageChange = (page, pageSize) => {
+        setPagination(prev => ({
+            ...prev,
+            current: page,
+            pageSize: pageSize
+        }));
+        window.scrollTo(0, 0);
+    };
 
-  return (
-    <div className="container">
-      <Row gutter={24}>
-        <Col span={6} className={"border-end mt-5"}>
-          <div>
-            <Typography.Title level={5}>Tìm kiếm</Typography.Title>
-            <SearchInput className={"mt-5"} onSearch={onSearch} />
-          </div>
-          <Divider />
-        </Col>
+    const onSearch = value => {
+        setSearch(value);
+    };
 
-        <Col span={18}>
-          <Row gutter={[16, 16]}>
-            {profile &&
-              profile.map((profile, index) => (
-                <Col key={profile.id || index} span={6}>
-                  <FreelancerCard profile={profile} />
+    return (
+        <div className="container">
+            <Row gutter={24}>
+                <Col span={6} className={"border-end mt-5"}>
+                    <div>
+                        <Typography.Title level={5}>Tìm kiếm</Typography.Title>
+                        <SearchInput className={"mt-5"} onSearch={onSearch} />
+                    </div>
+                    <Divider />
                 </Col>
-              ))}
 
-            {(!profile || profile?.length == 0) && (
-              <div>
-                {Array.from({ length: 10 }).map((_, index) => (
-                  <Skeleton
-                    avatar
-                    key={index}
-                    active={true}
-                    paragraph={{
-                      rows: 4,
-                    }}
-                  />
-                ))}
-                <Skeleton.Input className={"ms-auto d-block"} active={true} />
-              </div>
-            )}
-          </Row>
-          {profile && (
-            <Pagination
-              pagination={pagination}
-              align="end"
-              onChange={handlePageChange}
-            ></Pagination>
-          )}
-        </Col>
-      </Row>
-    </div>
-  );
+                <Col span={18}>
+                    <Row gutter={[16, 16]}>
+                        {profile &&
+                            profile.map((profile, index) => (
+                                <Col key={profile.id || index} span={6}>
+                                    <FreelancerCard profile={profile} />
+                                </Col>
+                            ))}
+
+                        {(!profile || profile?.length == 0) && (
+                            <div>
+                                {Array.from({ length: 10 }).map((_, index) => (
+                                    <Skeleton
+                                        avatar
+                                        key={index}
+                                        active={true}
+                                        paragraph={{
+                                            rows: 4
+                                        }}
+                                    />
+                                ))}
+                                <Skeleton.Input className={"ms-auto d-block"} active={true} />
+                            </div>
+                        )}
+                    </Row>
+                    {profile && (
+                        <Pagination
+                            pageSize={pagination.pageSize}
+                            current={pagination.current}
+                            total={pagination.total}
+                            align="end"
+                            onChange={handlePageChange}
+                        ></Pagination>
+                    )}
+                </Col>
+            </Row>
+        </div>
+    );
 };
 
 export default ListFreelancer;
