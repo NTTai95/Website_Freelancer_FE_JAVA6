fafe4dc5e664690ec0349457c9d6563861688300
VuVoVan <vud87099@gmail.com>
2025-04-20 09:06:27 +0700

    123

--- src/api/languageApi.js
+++ src/api/languageApi.js
@@ -1,7 +1,7 @@
 import apiClient from './index';
 
 const languageApi = {
-  getAll: ({ page, size } = {}) => apiClient.get('/languages/all', { params: { page, size } }),
+  getAll: ({ page, size, name, iso }) => apiClient.get('/languages', { params: { page, size, search: `name,${name || ""},iso,${iso || ""}` } }),
   getById: (id) => apiClient.get(`/languages/${id}`),
   getByIds: (ids) => apiClient.get(`/languages/list`, { params: { ids: ids.join(',') } }),
   add: (language) => apiClient.post('/languages', language),
